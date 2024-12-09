7f5497f53b69764516fb1b7b1993b4a5553aeb82
zethorix <charleslien97@gmail.com>
2024-12-09 00:13:57 -0800

    Fix unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { InfoHeader } from './components/InfoHeader'
 import { Problem, SortKey, SortDirection } from './types'
 import { SearchFilters } from './components/SearchFilters'
